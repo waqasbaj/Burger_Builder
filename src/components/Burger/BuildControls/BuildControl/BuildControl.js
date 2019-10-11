dfdbb511eb784fc3fcb71f9760ff6cddc0075db8
System Administrator <root@Waqass-MacBook-Pro.local>
2019-10-10 22:10:32 -0400

    Added Build Control Buttons

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import classes from './BuildControl.css'
 
-const buildControl = (props) =>{
+const buildControl = (props) =>(
 
+    
     <div className = {classes.BuildControl}>
-        <div className = {classes.label}>{props.label}</div>
-        <buttom className={classes.Less}>Less</buttom>
-        <buttom className={classes.More}>More</buttom>
+        <div className = {classes.Label}>{props.label}</div>
+        <button className={classes.Less}>Less</button>
+        <button className={classes.More}>More</button>
     </div>
-}
+    
+)
 
 export default buildControl
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl'
+import BurgerIngredient from '../BurgerIngredient/BurgerIngredient'
+
+const controls = [
+    {label : 'Salad', type: 'salad'},
+    {label : 'Bacon', type: 'bacon'},
+    {label : 'Cheese', type: 'cheese'},
+    {label : 'Meat', type: 'meat'}
+]
 
 const buildControls = (props) => (
 
-    <div>
-        
+    <div className = {classes.BuildControls}>
+
+        {controls.map(ctrl =>(
+            <BuildControl key ={ctrl.label} label={ctrl.label}/>
+        ))}
+
     </div>
 )
 
