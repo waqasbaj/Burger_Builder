5183cf4ecd70317786a716a3a0729989dd6963ed
System Administrator <root@Waqass-MacBook-Pro.local>
2019-10-15 00:05:43 -0400

    Added Logo, SideDrawer

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import burgerLogo from '../../assets/Images/burger-logo.png'
+import classes from './Logo.css'
+
+const logo = (props) =>(
+    <div className = {classes.Logo}>
+        <img src= {burgerLogo} alt='BIG-BAD-BURGER'/>
+    </div>
+)
+
+
+export default logo 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import classes from './Toolbar.css'
+import Logo from '../../Logo/Logo'
+import Navigation from '../NavigationItems/NavigationItems'
+
 
 const toolbar = (props) =>(
     <header className={classes.Toolbar}>
         <div> MENU</div>
-        <div>LOGO</div>
+        <div className = {classes.Logo}>
+             <Logo />
+        </div>
         <nav>
             <ul>
-                ...
+                <Navigation/>
             </ul>
         </nav>
     </header>
